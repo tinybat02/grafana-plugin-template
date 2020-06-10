1e03327bfba99d2a008391d36ad29b53612cf4a0
Nam Le <nam.leduc02@gmail.com>
2020-06-10 15:02:43 +0200

    Readjust radius

--- src/MainPanel.tsx
+++ src/MainPanel.tsx
@@ -1,46 +1,64 @@
 import React, { PureComponent } from 'react';
 import { PanelProps } from '@grafana/data';
-import { PanelOptions } from 'types';
+import { PanelOptions, Frame } from 'types';
+import StopIcon from './img/stop-icon.png';
+import GoIcon from './img/go-icon.png';
+import { CircularProgressbarWithChildren } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
 
 interface Props extends PanelProps<PanelOptions> {}
+interface State {
+  num: number;
+}
 
-export class MainPanel extends PureComponent<Props> {
+export class MainPanel extends PureComponent<Props, State> {
+  state: State = {
+    num: 0,
+  };
+
+  componentDidMount() {
+    console.log('customer count', this.props.data);
+    const series = this.props.data.series as Frame[];
+    if (series.length == 0 || series[0].fields[0].values.buffer.length == 0) {
+      return;
+    }
+
+    this.setState({ num: series[0].fields[0].values.buffer[0] });
+  }
+
+  componentDidUpdate(prevProps: Props) {
+    if (prevProps.data.series !== this.props.data.series) {
+      const series = this.props.data.series as Frame[];
+      if (series.length == 0 || series[0].fields[0].values.buffer.length == 0) {
+        return;
+      }
+
+      this.setState({ num: series[0].fields[0].values.buffer[0] });
+    }
+  }
+
   render() {
     const { width, height } = this.props;
+    const { num } = this.state;
 
     return (
       <div
         style={{
-          position: 'relative',
           width,
           height,
+          padding: 10,
         }}
       >
-        <svg
-          style={{
-            position: 'absolute',
-            top: 0,
-            left: 0,
-          }}
-          width={width}
-          height={height}
-          xmlns="http://www.w3.org/2000/svg"
-          xmlnsXlink="http://www.w3.org/1999/xlink"
-          viewBox={`-${width / 2} -${height / 2} ${width} ${height}`}
-        >
-          <g>
-            <circle style={{ fill: '#32a852' }} r={100} />
-          </g>
-        </svg>
-
-        <div
-          style={{
-            position: 'absolute',
-            bottom: 0,
-            left: 0,
-            padding: '10px',
-          }}
-        ></div>
+        <CircularProgressbarWithChildren value={num > 10 ? 100 : (num / 10) * 100}>
+          {num < 7 ? (
+            <img style={{ width: width / 2.2, transform: 'translateY(5px)' }} src={GoIcon} />
+          ) : (
+            <img style={{ width: width / 2.2, transform: 'translateY(5px)' }} src={StopIcon} />
+          )}
+          <div style={{ fontSize: 16, transform: 'translateY(10px)' }}>
+            <strong>{num}</strong>
+          </div>
+        </CircularProgressbarWithChildren>
       </div>
     );
   }
--- src/types.ts
+++ src/types.ts
@@ -1,7 +1,17 @@
-export interface PanelOptions {
+import { DataFrame, Field, Vector } from '@grafana/data';
 
+export interface PanelOptions {}
+
+export const defaults: PanelOptions = {};
+
+export interface Buffer extends Vector {
+  buffer: any;
 }
 
-export const defaults: PanelOptions = {
+export interface FieldBuffer extends Field<any, Vector> {
+  values: Buffer;
+}
 
-};
+export interface Frame extends DataFrame {
+  fields: FieldBuffer[];
+}
