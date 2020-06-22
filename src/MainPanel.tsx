86a8750a139f2017c06e85bef29735fa8fa77bb3
Nam Le <nam.leduc02@gmail.com>
2020-06-22 14:24:47 +0200

    Restore code

--- src/MainPanel.tsx
+++ src/MainPanel.tsx
@@ -1,45 +1,17 @@
 import React, { PureComponent } from 'react';
 import { PanelProps } from '@grafana/data';
-import { PanelOptions, Frame } from 'types';
-import StopIcon from './img/stop-icon.png';
-import GoIcon from './img/go-icon.png';
-import { CircularProgressbarWithChildren } from 'react-circular-progressbar';
-import 'react-circular-progressbar/dist/styles.css';
+import { PanelOptions } from 'types';
 
 interface Props extends PanelProps<PanelOptions> {}
-interface State {
-  num: number;
-}
+interface State {}
 
 export class MainPanel extends PureComponent<Props, State> {
-  state: State = {
-    num: 0,
-  };
+  componentDidMount() {}
 
-  componentDidMount() {
-    console.log('customer count', this.props.data);
-    const series = this.props.data.series as Frame[];
-    if (series.length == 0 || series[0].fields[0].values.buffer.length == 0) {
-      return;
-    }
-
-    this.setState({ num: series[0].fields[0].values.buffer[0] });
-  }
-
-  componentDidUpdate(prevProps: Props) {
-    if (prevProps.data.series !== this.props.data.series) {
-      const series = this.props.data.series as Frame[];
-      if (series.length == 0 || series[0].fields[0].values.buffer.length == 0) {
-        return;
-      }
-
-      this.setState({ num: series[0].fields[0].values.buffer[0] });
-    }
-  }
+  componentDidUpdate(prevProps: Props) {}
 
   render() {
     const { width, height } = this.props;
-    const { num } = this.state;
 
     return (
       <div
@@ -49,16 +21,7 @@ export class MainPanel extends PureComponent<Props, State> {
           padding: 10,
         }}
       >
-        <CircularProgressbarWithChildren value={num > 10 ? 100 : (num / 10) * 100}>
-          {num < 7 ? (
-            <img style={{ width: width / 2.2, transform: 'translateY(5px)' }} src={GoIcon} />
-          ) : (
-            <img style={{ width: width / 2.2, transform: 'translateY(5px)' }} src={StopIcon} />
-          )}
-          <div style={{ fontSize: 16, transform: 'translateY(10px)' }}>
-            <strong>{num}</strong>
-          </div>
-        </CircularProgressbarWithChildren>
+        <h1>Hello</h1>
       </div>
     );
   }
